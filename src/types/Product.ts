411b81386b8d0d4de4d6056cd24aeedf44e2538e
José Ángel Hernández <joseangel@Mac-mini-de-Jose.local>
2025-05-19 11:42:06 +0200

    feat: extended product type to reuse on product detail

--- src/types/Product.ts
+++ src/types/Product.ts
@@ -1,7 +1,25 @@
-export interface Product {
+// Basic product properties
+export interface ProductSummary {
   id: string;
   brand: string;
   model: string;
   price: string;
   imgUrl: string;
+}
+
+// Extended product properties
+export interface ProductDetail extends ProductSummary {
+  cpu: string;
+  ram: string;
+  os: string;
+  battery: string;
+  displayResolution: string;
+  dimentions: string;
+  weight: string;
+  primaryCamera?: string;
+  secondaryCamera?: string;
+  options: {
+    colors: { code: string; name: string }[];
+    storages: { code: string; name: string }[];
+  };
 }
