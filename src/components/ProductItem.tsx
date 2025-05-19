2179aebecfef76397bf24825b3be2837565a2a40
José Ángel Hernández <joseangel@Mac-mini-de-Jose.local>
2025-05-19 11:16:57 +0200

    feat: add ProductItem component for product Listing

---
+++ src/components/ProductItem.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { Product } from "../types/Product";
+
+interface Props {
+  product: Product;
+}
+
+const ProductItem = ({ product }: Props) => {
+  return (
+    <Link
+      href={`/product/${product.id}`}
+      className="block border rounded p-4 hover:shadow transition"
+    >
+      <Image
+        src={product.imgUrl}
+        alt={product.model}
+        width={300}
+        height={160}
+        className="w-full h-40 object-contain mb-2"
+        priority={false} // Lazy load by default
+      />
+
+      <div>
+        <h2 className="font-semibold text-lg">
+          {product.brand} {product.model}
+        </h2>
+        <p className={product.price ? "text-blue-600" : "text-gray-400"}>
+          {product.price ? `${product.price} €` : "Not available"}
+        </p>
+      </div>
+    </Link>
+  );
+};
+
+export default ProductItem;
